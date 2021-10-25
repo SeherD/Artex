b24a753b0784a67a928a1d3845398eb6b64e783c
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2021-10-25 09:27:37 -0600

    update framework and .gitignore

--- Artex/obj/Debug/net48/App.g.i.cs
+++ Artex/obj/Debug/net48/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Artex {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Artex {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public static void Main() {
             Artex.App app = new Artex.App();
             app.InitializeComponent();
