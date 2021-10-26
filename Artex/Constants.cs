f9f0ad94e111e5fa8c2075a4c51ddabf8f5d5dab
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2021-10-26 12:22:29 -0600

    Add color constants

---
+++ Artex/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+static class Constants
+{
+    public const string LightPink   = "#ffc0c0";
+    public const string CamoGreen   = "#71a18c";
+    public const string DarkPeach   = "#ff978d";
+    public const string Beige       = "#ffc9a6";
+    public const string LightBeige  = "#ffe6c3";
+    public const string Mauve       = "643cff";
+    public const string Grey        = "c3c7d9";
+}
