bd4fc381658c74dd5be8f12a655fa608066e621d
Seher Dawar <59703840+SeherD@users.noreply.github.com>
2021-12-04 16:03:28 -0700

    Shop complete, Library has search and tags

--- Shop/EditShopPage.xaml.cs
+++ Shop/EditShopPage.xaml.cs
@@ -21,50 +21,256 @@ namespace Shop
         public EditShopPage()
         {
             InitializeComponent();
-        }
-        void RightMove(object sender, RoutedEventArgs e)
-        {
-            if (Image1.Visibility == Visibility.Visible)
+            leftcount = ImageHolder.Children.Count-1;
+            for (int i = 1; i < ImageHolder.Children.Count; i++)
             {
-                Image1.Visibility = Visibility.Hidden;
-                Image2.Visibility = Visibility.Visible;
-                Image3.Visibility = Visibility.Hidden;
-            }
-            else if (Image2.Visibility == Visibility.Visible)
-            {
-                Image1.Visibility = Visibility.Hidden;
-                Image2.Visibility = Visibility.Hidden;
-                Image3.Visibility = Visibility.Visible;
-            }
-            else if (Image3.Visibility == Visibility.Visible)
-            {
-                Image1.Visibility = Visibility.Visible;
-                Image2.Visibility = Visibility.Hidden;
-                Image3.Visibility = Visibility.Hidden;
-            }
-        }
+                if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                {
+                    StackPanel image = (StackPanel)ImageHolder.Children[i];
+                    image.Visibility = Visibility.Collapsed;
+                }
 
+                
+            }
+        }
+        bool sort = false;
+
+        int rightcount=0;
+        void RightMove(object sender, RoutedEventArgs e)
+        { rightcount++;
+
+            if(rightcount== ImageHolder.Children.Count)
+            {
+                rightcount = 0;
+            }
+
+            if (!sort) {
+                for (int i = 0; i < ImageHolder.Children.Count; i++)
+                {
+                    if (i == rightcount)
+                    {
+                        if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)ImageHolder.Children[i];
+                            image.Visibility = Visibility.Visible;
+                        }
+                    }
+
+                    else
+                    {
+                        if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)ImageHolder.Children[i];
+                            image.Visibility = Visibility.Collapsed;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < SortedPrice.Children.Count; i++)
+                {
+                    if (i == rightcount)
+                    {
+                        if (SortedPrice.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)SortedPrice.Children[i];
+                            image.Visibility = Visibility.Visible;
+                        }
+                    }
+
+                    else
+                    {
+                        if (SortedPrice.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)SortedPrice.Children[i];
+                            image.Visibility = Visibility.Collapsed;
+                        }
+
+                    }
+                }
+            }
+        }
+        int leftcount;
         void LeftMove(object sender, RoutedEventArgs e)
         {
-            if (Image1.Visibility == Visibility.Visible)
+            leftcount--;
+            if (leftcount == -1)
             {
-                Image1.Visibility = Visibility.Hidden;
-                Image3.Visibility = Visibility.Visible;
-                Image2.Visibility = Visibility.Hidden;
+                leftcount = ImageHolder.Children.Count-1;
             }
-            else if (Image2.Visibility == Visibility.Visible)
+
+            if (!sort)
             {
-                Image2.Visibility = Visibility.Hidden;
-                Image3.Visibility = Visibility.Hidden;
-                Image1.Visibility = Visibility.Visible;
+                for (int i = 0; i < ImageHolder.Children.Count; i++)
+                {
+                    if (i == leftcount)
+                    {
+                        if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)ImageHolder.Children[i];
+                            image.Visibility = Visibility.Visible;
+                        }
+                    }
+
+                    else
+                    {
+                        if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)ImageHolder.Children[i];
+                            image.Visibility = Visibility.Collapsed;
+                        }
+
+                    }
+                }
+
             }
-            else if (Image3.Visibility == Visibility.Visible)
+            else
             {
-                Image2.Visibility = Visibility.Visible;
-                Image1.Visibility = Visibility.Hidden;
-                Image3.Visibility = Visibility.Hidden;
+                for (int i = 0; i < SortedPrice.Children.Count; i++)
+                {
+                    if (i == leftcount)
+                    {
+                        if (SortedPrice.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)SortedPrice.Children[i];
+                            image.Visibility = Visibility.Visible;
+                        }
+                    }
+
+                    else
+                    {
+                        if (SortedPrice.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)SortedPrice.Children[i];
+                            image.Visibility = Visibility.Collapsed;
+                        }
+
+                    }
+                }
+            }
+            
+        }
+
+
+        void search(object sender, TextChangedEventArgs args)
+        {
+            if (SearchBox.Text.Contains("Five Star"))
+            {
+                RightButton.IsEnabled = false;
+                LeftButton.IsEnabled = false;
+                for (int i = 1; i < ImageHolder.Children.Count; i++)
+                {
+                    if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                    {
+                        StackPanel image = (StackPanel)ImageHolder.Children[i];
+                        image.Visibility = Visibility.Collapsed;
+                    }
+
+
+                }
+            }
+
+            else if (SearchBox.Text.Contains("Color"))
+            {
+                RightButton.IsEnabled = false;
+                LeftButton.IsEnabled = false;
+                for (int i = 0; i < ImageHolder.Children.Count; i++)
+                {
+                    if (i == 4)
+                    {
+                        if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)ImageHolder.Children[i];
+                            image.Visibility = Visibility.Visible;
+                        }
+                    }
+
+                    else
+                    {
+                        if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                        {
+                            StackPanel image = (StackPanel)ImageHolder.Children[i];
+                            image.Visibility = Visibility.Collapsed;
+                        }
+
+                    }
+                }
+
+            }
+            else
+            {
+                RightButton.IsEnabled = true;
+                LeftButton.IsEnabled = true;
+
+            }
+
+        }
+
+
+        void tag1(object sender, RoutedEventArgs e)
+        {
+            RightButton.IsEnabled = false;
+            LeftButton.IsEnabled = false;
+            for (int i = 1; i < ImageHolder.Children.Count; i++)
+            {
+                if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                {
+                    StackPanel image = (StackPanel)ImageHolder.Children[i];
+                    image.Visibility = Visibility.Collapsed;
+                }
+
+
             }
         }
 
+        void tag2(object sender, RoutedEventArgs e)
+        {
+            RightButton.IsEnabled = false;
+            LeftButton.IsEnabled = false;
+            for (int i = 0; i < ImageHolder.Children.Count; i++)
+            {
+                if (i == 2)
+                {
+                    if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                    {
+                        StackPanel image = (StackPanel)ImageHolder.Children[i];
+                        image.Visibility = Visibility.Visible;
+                    }
+                }
+
+                else
+                {
+                    if (ImageHolder.Children[i].GetType().Name == "StackPanel")
+                    {
+                        StackPanel image = (StackPanel)ImageHolder.Children[i];
+                        image.Visibility = Visibility.Collapsed;
+                    }
+
+                }
+            }
+        }
+
+        void uncheck(object sender, RoutedEventArgs e) {
+            RightButton.IsEnabled = true;
+            LeftButton.IsEnabled = true;
+        }
+
+        void sortedPrice(object sender, RoutedEventArgs e)
+        {
+            ImageHolder.Visibility = Visibility.Collapsed;
+            SortedPrice.Visibility = Visibility.Visible;
+            sort = true;
+
+        }
+
+        void unsortedPrice(object sender, RoutedEventArgs e)
+        {
+            ImageHolder.Visibility = Visibility.Visible;
+            SortedPrice.Visibility = Visibility.Collapsed;
+            sort = false;
+
+        }
     }
 }
